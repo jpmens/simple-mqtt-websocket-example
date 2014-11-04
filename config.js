24ca3219c684b666a8c459110d00557a675e9822
Jan-Piet Mens <jpmens@gmail.com>
2014-11-04 17:49:58 +0100

    paho.js develop (pre 1.1)

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // host = '172.16.153.122';	// hostname or IP address
-// host = '127.0.0.1';	// hostname or IP address
-host = '172.16.153.110';	// hostname or IP address
+host = '127.0.0.1';	// hostname or IP address
+// host = '172.16.153.110';	// hostname or IP address
 port = 9001;
 topic = '#';		// topic to subscribe to
 useTLS = false;

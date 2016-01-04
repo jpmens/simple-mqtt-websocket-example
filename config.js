88ee493ea4147e2dbf4a1f430bd44cd38c05743c
Christoph Krey <krey.christoph@gmail.com>
2016-01-04 10:20:28 +0100

    [NEW] use optional path argument for websocket URL

--- config.js
+++ config.js
@@ -8,4 +8,12 @@ username = null;
 password = null;
 // username = "jjolie";
 // password = "aa";
+
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
 cleansession = true;
